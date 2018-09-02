08e69123c18a447272a8ff7d2f38dda94ca8001d
iCeMan <fabiodiasfrancisco27@gmail.com>
2018-09-02 12:25:12 +0100

    bumped dependencies versions; bumped own version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject car-cpu-clj "2.0.0"
+(defproject car-cpu-clj "2.0.1"
   :description "Clojure library to handle communication between the Car CPU and the dashboard"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.442"]
-                 [car-data-clj "2.0.0"]
+                 [car-data-clj "2.0.1"]
                  [overtone/at-at "1.2.0"]
                  [pt.iceman/carscreentools "0.0.1"]
                  [car-ai-clj "1.0.0"]]
